9b1b8eb642745c670649fded60e12625c8c175c8
adham <adamtarek61@gmail.com>
2020-05-31 04:09:58 +0200

    first commit

---
+++ validation.js
@@ -0,0 +1,29 @@
+const inputs = document.querySelectorAll('input');
+
+// regex patterns
+const patterns = {
+        telephone: /^\d{11}$/,
+        username: /^[a-z\d]{5,12}$/i,
+        password: /^[\w@!-]{8,20}$/,
+        slug: /^[a-z\d-]{8,20}$/,
+        email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+};
+
+// validation function
+function validate(field, regex){
+
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+
+}
+
+// attach keyup events to inputs
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+            // console.log(patterns[e.target.attributes.name.value]);
+            validate(e.target, patterns[e.target.attributes.name.value]);
+    });
+});
